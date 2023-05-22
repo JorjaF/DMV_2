0e6ee9e283e5096efb4653e9539321a6d595c0ee
Jorja Fleming <aset284@gmail.com>
2023-05-22 14:18:32 -0600

    Work on Facility factory

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -8,14 +8,6 @@ class FacilityFactory
    Facility.new(facility)
   end
 
-  # def create_or_facility(facility)
-  #   facility = facility.slice(:title,:location_1, :phone_number)
-  #   facility[:name] = facility.delete(:title)
-  #   facility[:address] = parse_or_address(facility.delete(:location_1))
-  #   facility[:phone] = facility.delete(:phone_number)
-  #   Facility.new(facility)
-  # end
-
   def parse_or_address(raw_address)
     raw = raw_address[:human_address]
     parsed_address = JSON.parse(raw)
